8327faad1c7261e4e9aa78e58ba2dd59b1c21565
Amit Arazi <amitarazi123456@gmail.com>
2023-05-09 12:58:57 +0300

    access control

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config({ path: __dirname + "/.env" });
 require("./src/db/mongoose");
+require("dotenv").config({ path: __dirname + "/.env" });
 const userRouter = require("./src/routers/user");
 
 const app = express();
@@ -9,14 +9,15 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(userRouter);
 
-app.get("/", (req, res) => {
-  res.send({ message: "working" });
-});
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
+
+app.get("/", (req, res) => {
+  res.send({ message: "working" });
+});
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
